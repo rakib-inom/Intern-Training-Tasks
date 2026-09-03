51aa3dcbe947f42e4e2a71db3e128ef6ce262d51
OwnFuture <faisalmithun1316@gmail.com>
2026-09-03 23:50:46 +0600

    Interface code added

---
+++ Week_1/LibraryManagement/ILibraryItem.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagement
+{
+    public interface ILibraryItem
+    {
+        string Title { get; set; }
+        bool IsAvailable { get; }
+        
+        void CheckOut();
+        void Return();
+        void Describe();
+    }
+}
