99b655a2a0ecafdec415542d6b6988613486767e
Rabu <raburabeya042@gmail.com>
2026-09-04 21:18:35 +0600

    Add Library Management project

---
+++ week-1/LibraryManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
