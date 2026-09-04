3c298a8feecf0bef8161b68c67664b42bb29dde9
OwnFuture <faisalmithun1316@gmail.com>
2026-09-04 20:16:00 +0600

    Abstract LibraryItemBase class added upto constructor

---
+++ Week_1/LibraryManagement/LibraryItemBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LibraryManagement
+{
+    public abstract class LibraryItemBase : ILibraryItem
+    {
+        private string _title; 
+        private bool _isAvailable;
+
+        public string Title
+        {
+            get { return _title; }
+
+            set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Title cannot be null or empty.");
+                }
+                _title = value;
+            }
+        }
+
+        public bool IsAvailable
+        {
+            get { return _isAvailable; }
+            
+        }
+
+        protected LibraryItemBase(string title)
+        {
+            Title = title;
+            _isAvailable = true;
+        }
+    }
+}
