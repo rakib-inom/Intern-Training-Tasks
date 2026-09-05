47e482c7794f240b779819758f7d97a3f42ea344
OwnFuture <faisalmithun1316@gmail.com>
2026-09-05 23:42:30 +0600

    Book class added

---
+++ Week_1/LibraryManagement/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LibraryManagement
+{
+    public class Book : LibraryItemBase
+    {
+        private string _author;
+
+        public string Author
+        {
+            get { return _author; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Author cannot be null or empty.");
+                }
+                _author = value;
+            }
+        }
+
+        public Book(string title, string author) : base(title)
+        {
+            Author = author;
+        }
+
+        public override void Describe()
+        {
+            Console.WriteLine($"Book: {Title} by {Author}");
+        }
+    }
+}
