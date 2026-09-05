47c8a251c13c45abe25d6d1208cc1d6883f293d0
sanzid943 <sanzidmostofa@gmail.com>
2026-09-05 11:31:47 +0600

    add library branch info class

---
+++ Week-1/Library_Management/LibraryBranchInfo.cs
@@ -0,0 +1,13 @@
+
+public struct LibraryBranchInfo
+{
+    public string BrachCode;
+    public string location;
+
+    public LibraryBranchInfo(string branchCode, string location)
+    {
+        BranchCode = BranchCode;
+        location = location;
+
+    } 
+}
