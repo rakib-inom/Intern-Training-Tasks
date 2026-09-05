5fc0ec36bc9d897131e43d7f8933cb670fa4bc5b
OwnFuture <faisalmithun1316@gmail.com>
2026-09-05 23:47:46 +0600

    Magazine class implemented

---
+++ Week_1/LibraryManagement/Magazine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LibraryManagement
+{
+    public class Magazine : LibraryItemBase
+    {
+        private int _issueNumber;
+        public int IssueNumber
+        {
+            get { return _issueNumber; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Issue number must be a positive integer.");
+                }
+                _issueNumber = value;
+            }
+        }
+        public Magazine(string title, int issueNumber) : base(title)
+        {
+            IssueNumber = issueNumber;
+        }
+        public override void Describe()
+        {
+            Console.WriteLine($"Magazine: {Title}, Issue Number: {IssueNumber}");
+        }
+    }
+}
