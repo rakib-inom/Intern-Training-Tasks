f1954e258b1d950862ca235a02dc96a25407a631
sanzid943 <sanzidmostofa@gmail.com>
2026-09-05 11:02:41 +0600

    add LibraryItemBase class

---
+++ Week-1/Library_Management/LibraryItemBase.cs
@@ -0,0 +1,31 @@
+public abstract class LibraryItemBase : ILibraryItem
+{
+    public string Title { get; set; }
+
+    public bool isAvailable { get; protected set; }
+
+    protected string itemType;
+
+    public LibraryItemBase(string title)
+    {
+        Title = title;
+        isAvailable = true;
+    }
+
+    public void checkOut()
+    {
+        isAvailable = false;
+
+        console.WriteLine(Title + " has been checked out.");
+    }
+
+    public void Return()
+    {
+        isAvailable = true;
+
+        console.WriteLine(Title + " has been returned.");
+    }
+
+    public abstract void describe();
+
+}
