a01f145a73f3e69398001c2537331c96293ed1f1
sanzid943 <sanzidmostofa@gmail.com>
2026-09-05 11:13:40 +0600

    add book library item class

---
+++ Week-1/Library_Management/Book.cs
@@ -0,0 +1,20 @@
+public class Book : LibraryItemBase
+{
+    public string Author { get; set; }
+    
+    public Book(string title, string author) : base(title)
+    {
+        Author = author;
+
+        itemType = "Book";
+    }
+
+    public override void describe()
+    {
+        console.WriteLine("Book")
+        console.WriteLine("Title: " + Title);
+        console.WriteLine("Author: " + Author);
+        console.WriteLine("Available: " + isAvailable);
+        console.WriteLine();
+    }
+}
