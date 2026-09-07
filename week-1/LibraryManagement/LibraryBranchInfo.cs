3ed827d3c800da3dd00f96702f38f04d933cd254
Rabu <raburabeya042@gmail.com>
2026-09-07 10:53:27 +0600

    Add value and reference type demonstration

---
+++ week-1/LibraryManagement/LibraryBranchInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagement
+{
+    public struct LibraryBranchInfo
+    {
+        public string BranchCode { get; set; }
+        public string Location { get; set; }
+    }
+}
--- week-1/LibraryManagement/Program.cs
+++ week-1/LibraryManagement/Program.cs
@@ -21,6 +21,30 @@ namespace LibraryManagement
             book.Return();
 
             Console.WriteLine($"Book Available after return: {book.IsAvailable}");
+
+            // Value Type demonstration
+            LibraryBranchInfo branch1 = new LibraryBranchInfo
+            {
+                BranchCode = "BR001",
+                Location = "Dhaka"
+            };
+
+            LibraryBranchInfo branch2 = branch1;
+            branch2.Location = "Chittagong";
+
+            Console.WriteLine("\nValue Type Demonstration:");
+            Console.WriteLine($"Original Branch Location: {branch1.Location}");
+            Console.WriteLine($"Copied Branch Location: {branch2.Location}");
+
+            // Reference Type demonstration
+            Book originalBook = new Book("Clean Code", "Robert C. Martin");
+
+            Book copiedBook = originalBook;
+            copiedBook.Author = "Changed Author";
+
+            Console.WriteLine("\nReference Type Demonstration:");
+            Console.WriteLine($"Original Book Author: {originalBook.Author}");
+            Console.WriteLine($"Copied Book Author: {copiedBook.Author}");
         }
     }
 }
