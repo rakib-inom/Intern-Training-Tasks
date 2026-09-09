f81f7a22b98c33c02260f34e3a8c592d4620c5f9
sanzid943 <sanzidmostofa@gmail.com>
2026-09-10 00:18:05 +0600

    add IIdentifiable interface

--- Week-1/Library_Management/Book.cs
+++ Week-1/Library_Management/Book.cs
@@ -1,12 +1,15 @@
-public class Book : LibraryItemBase
+public class Book : LibraryItemBase, IIdentifiable
 {
     public string Author { get; set; }
+    public string Category { get; set; }
+    public string ID { get; }
     
-    public Book(string title, string author) : base(title)
+    public Book(string title, string author, string category, string id) : base(title)
     {
         Author = author;
-
         ItemType = "Book";
+        Category = category;
+        ID= id;
     }
 
     public override void Describe()
@@ -14,8 +17,10 @@ public class Book : LibraryItemBase
         Console.WriteLine();
         Console.WriteLine("BOOK ");
         Console.WriteLine();
+        Console.WriteLine("ID: " + ID);
         Console.WriteLine("Title: " + Title);
         Console.WriteLine("Author: " + Author);
+        Console.WriteLine("Category: " + Category);
         Console.WriteLine("Available: " + IsAvailable);
         Console.WriteLine();
     }
---
+++ Week-1/Library_Management/IIdentifiable.cs
@@ -0,0 +1,4 @@
+public interface IIdentifiable
+{
+    string ID { get; }
+}
