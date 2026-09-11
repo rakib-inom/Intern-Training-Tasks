196b400c52d1362d988ae0410173907b92ca127e
sanzid943 <sanzidmostofa@gmail.com>
2026-09-11 14:43:07 +0600

    update library item base file

--- Week-1/Library_Management/LibraryItemBase.cs
+++ Week-1/Library_Management/LibraryItemBase.cs
@@ -1,12 +1,11 @@
 
-
 public abstract class LibraryItemBase : ILibraryItem
 {
-    public string Title { get; set; }
+    public string Title { get; private set; }
 
     public bool IsAvailable { get; protected set; }
 
-    protected string ItemType;
+    protected string ItemType( get; set; );
 
     public LibraryItemBase(string title)
     {
@@ -17,16 +16,24 @@ public abstract class LibraryItemBase : ILibraryItem
     public void CheckOut()
     {
   
-        IsAvailable = false;
+        if (IsAvailable)
+        {
+            IsAvailable = false;
 
-        Console.WriteLine(Title + " is checked out.");
+            Console.WriteLine(Title + " has been checked out.");
+
+        }
+        else
+        {
+            Console.WriteLine(Title + " is already checked out.")
+        }
     }
 
     public void Return()
     {
         IsAvailable = true;
 
-        Console.WriteLine(Title + " is returned.");
+        Console.WriteLine(Title + " has been returned.");
     }
 
     public abstract void Describe();
