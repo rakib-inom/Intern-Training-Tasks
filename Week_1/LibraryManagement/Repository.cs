f8cd2a3f2e1e8a42fe844897731a51c8ede46de9
OwnFuture <faisalmithun1316@gmail.com>
2026-09-13 12:12:00 +0600

    Repository class added

---
+++ Week_1/LibraryManagement/Repository.cs
@@ -0,0 +1,45 @@
+﻿namespace LibraryManagement
+{
+    public class Repository<T> where T : IIdentifiable
+    {
+        private readonly List<T> _items;
+        private readonly Dictionary<string, T> _itemsById;
+
+        public Repository()
+        {
+            _items = new List<T>();
+            _itemsById = new Dictionary<string, T>();
+        }
+
+        public void Add(T item)
+        {
+            if (_itemsById.ContainsKey(item.Id))
+            {
+                throw new DuplicateItemException($"An item with ID '{item.Id}' already exists.");
+            }
+            _items.Add(item);
+            _itemsById.Add(item.Id, item);
+        }
+
+        public List<T> GetAll()
+        {
+            return _items;
+        }
+
+        public T GetById(string id)
+        {
+            if (!_itemsById.TryGetValue(id, out T? item))
+            {
+                throw new NotFoundException($"Item with ID '{id}' not found.");
+            }
+            return item;
+        }
+
+        public void Remove(string id)
+        {
+            T item = GetById(id);
+            _items.Remove(item);
+            _itemsById.Remove(item.Id);
+        }
+    }
+}
