cf8c2e5bb101cbf55babffe16c93c2e7945b4828
OwnFuture <faisalmithun1316@gmail.com>
2026-09-13 11:54:30 +0600

    Not found exception class added

---
+++ Week_1/LibraryManagement/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagement
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
