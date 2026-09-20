dfdc3e5b0a663f8890b1313ab6a7f258bde6005b
OwnFuture <faisalmithun1316@gmail.com>
2026-09-20 12:07:35 +0600

    Files are added in the list and started all downloads

--- Week_3/Async-Await/Program.cs
+++ Week_3/Async-Await/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Async-Await{
+    class program
+{
+    static Random random = new Random();
+
+    static async Task Main(string[] args)
+    {
+        List<string> files = new List<string> { "file1.txt", "file2.txt", "file3.txt" };
+
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        Console.WriteLine("Starting file downloads...");
+
+
+
+        List<Task<string>> downloadTasks = new List<Task<string>>();
+
+        foreach(string file in files)
+        {
+            Task<string> task = DownloadFileAsync(file);
+            downloadTasks.Add(task);
+        }
+    }
+}
+}
