554f93cecfe4cbc15f619feedcd8d7b38cb6f888
sanzid943 <sanzidmostofa@gmail.com>
2026-09-20 17:50:58 +0600

    add download file async method

--- Week-3/Asynchronous Programming/Program.cs
+++ Week-3/Asynchronous Programming/Program.cs
@@ -2,7 +2,10 @@
 {
     static async Task<string> DownloadFileAsync(string filename)
     {
-        
+        Console.WriteLine("Download started...");
+        await Task.Delay(2000);
+        Console.WriteLine("Download finished.");
+
     }
 
     static async Task<string> ProcessFileAsync(string contents)
